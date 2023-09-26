99677a8961ee95d0ac418e8962284f29398e3447
Andy Kim <andykim@Andys-MacBook-Air.local>
2023-09-27 01:05:52 +1300

    initial commit

---
+++ src/test/java/com/example/gitcommand/GitCommandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitcommand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitCommandApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
